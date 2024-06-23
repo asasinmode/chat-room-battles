09324e98c8bf5727b9a3c0afc917f934d78a8e04
asasinmode <skin9470@gmail.com>
2024-06-23 20:34:35 +0200

    docs: update description

--- app/app.vue
+++ app/app.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
 useSeoMeta({
 	title: 'Chat Room Battles',
-	description: 'A text-based auto-battler where you create messages with emojis, then send them to battle against other players',
+	description: 'A text-based auto-battler where you construct emoji messages then send them to battle with other players',
 	robots: {
 		none: true,
 	},
