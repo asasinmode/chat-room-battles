fa043fc710dc9bfcd9686473da96b347805d3670
asasinmode <skin9470@gmail.com>
2024-06-23 19:41:04 +0200

    feat: header html

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,15 +1,15 @@
 <template>
 	<main class="mx-auto h-full w-1/3 b-(x-2 black)">
-		<header class="h-10 flex items-center gap-3 b-(b-2 black) px-3">
+		<header class="h-12 flex items-center gap-3 b-(b-2 black) px-3">
 			<img src="https://loremflickr.com/200/200" class="size-6 rounded-1/2">
 			<h2 class="mr-auto text-5">
 				Party Animals
 			</h2>
-			<button title="menu" class="aspect-1 h-full flex-center -mr-3">
-				<div class="i-ph-list-bold" />
-				<span class="sr-only size-6">menu</span>
+			<button title="menu" class="aspect-1 w-10 flex-center rounded-1/2 -mr-3 hoverable:bg-gray-1">
+				<div class="i-ph-list-bold size-5" />
+				<span class="sr-only">menu</span>
 			</button>
-			<button title="information" class="aspect-1 h-full flex-center">
+			<button title="information" class="aspect-1 w-10 flex-center rounded-1/2 -mr-2 hoverable:bg-gray-1">
 				<div class="i-ph-info size-6" />
 				<span class="sr-only">information</span>
 			</button>
