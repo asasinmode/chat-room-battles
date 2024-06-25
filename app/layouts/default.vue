9d646d57b02bd42536a7f1b6ee96288c3ee21616
asasinmode <skin9470@gmail.com>
2024-06-25 18:06:08 +0200

    fix: alpha contrast

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -3,7 +3,7 @@
 		<header class="h-12 flex shrink-0 items-center gap-3 b-(b-2 black) px-2 dark:b-white">
 			<img src="/logo.webp" class="size-9">
 			<h2 class="mr-auto overflow-hidden whitespace-nowrap text-5 font-600">
-				Chat Room Battles <sup class="rounded bg-yellow-5 px-1 uppercase">alpha</sup>
+				Chat Room Battles <sup class="rounded bg-yellow-5 px-1 uppercase dark:bg-yellow-7">alpha</sup>
 			</h2>
 			<button
 				v-if="!$route.meta.isHomePage"
