05fed63b2cdb3c9f4986292b9d56a8ba76d58bb5
asasinmode <skin9470@gmail.com>
2024-07-01 20:20:16 +0200

    fix: unocss base button hoverable

--- uno.config.ts
+++ uno.config.ts
@@ -21,7 +21,7 @@ export default defineConfig({
 			'flex-center': 'flex justify-center items-center',
 			'translate-center': 'translate-x--1/2 translate-y--1/2',
 			'main-menu-link': 'h-12 font-600 leading-11 tracking-wider uppercase block px-2 text-center',
-			'base-button-hoverable': 'hoverable:bg-zinc-2 dark:hoverable:bg-zinc-8',
+			'base-button-hoverable': 'hover:bg-zinc-2 focus-visible:bg-zinc-2 dark:hover:bg-zinc-8 dark:focus-visible:bg-zinc-8',
 		},
 		[/^button[:-](.+)$/, ([, c]) => `px-3 py-1 font-600 uppercase rounded-lg bg-${c}`],
 	],
@@ -33,7 +33,9 @@ export default defineConfig({
 
 			return {
 				matcher: matcher.slice(10),
-				selector: s => `${s}:hover, ${s}:focus-visible`,
+				selector: (s) => {
+					return `${s}:hover, ${s}:focus-visible`;
+				},
 			};
 		},
 	],
